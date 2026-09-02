786e128ccb7578860994d9c76ddbee248295f98a
Aditya Raj Gupta <adityaraj51202@gmail.com>
2026-09-03 00:32:33 +0530

    envs

--- server/src/app.js
+++ server/src/app.js
@@ -12,8 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: process.env.CLIENT_URL,
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
+    // origin: "http://localhost:3000",
     credentials: true,
   })
 );
